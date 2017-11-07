e497e5397f27bd16c40dc67496daef9d1a7b6800
Daniel Brolli <daniel.brolli@gmail.com>
2017-11-07 12:20:15 +0100

    removed dev header

--- packages/assistify-ai/server/methods/getSmartiUiScript.js
+++ packages/assistify-ai/server/methods/getSmartiUiScript.js
@@ -17,13 +17,8 @@ function loadSmarti() {
 
 	let response = null;
 	try {
-		const options = {
-			headers: {
-				'Authorization': 'Basic YWRtaW46ZDJiYzkwYmEtMTM2MS00ZGFiLWExMTgtODc4NTNiOGY1ODE0'
-			}
-		};
 		SystemLogger.debug('Trying to retrieve Smarti-Widget script from', DBS_AI_SMARTI_WIDGET_URL);
-		response = HTTP.get(DBS_AI_SMARTI_WIDGET_URL, options);
+		response = HTTP.get(DBS_AI_SMARTI_WIDGET_URL);
 	} catch (error) {
 		SystemLogger.error('Could not load the Smarti-Widget script at', DBS_AI_SMARTI_URL);
 		SystemLogger.debug(error);
