667afb47cce2929d055b53ddca3e4e682889dc9d
Oliver Jägle <github@beimir.net>
2017-12-11 07:58:02 +0100

    Migrate settings

--- packages/assistify-ai/package.js
+++ packages/assistify-ai/package.js
@@ -27,6 +27,9 @@ Package.onUse(function(api) {
 	addDirectory(api, 'server/hooks', 'server');
 	addDirectory(api, 'server/methods', 'server');
 
+	//migration scripts
+	api.addFiles('server/migrations.js', 'server');
+
 	//Configuration
 	api.addFiles('config.js', 'server');
 
