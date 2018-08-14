9b87c8672216c1147770fa04ec7fd12050befc00
vickyokrm <vickyokrm@gmail.com>
2018-08-14 09:46:33 +0200

    method signature adjustment

--- packages/assistify-dbs-translation/server/methods/translate.js
+++ packages/assistify-dbs-translation/server/methods/translate.js
@@ -1,8 +1,8 @@
 
 Meteor.methods({
-	'assistify.translate'(httpMethod, url, params, data) {
+	'assistify.translate'(httpMethod, url, options) {
 		try {
-			const result = HTTP.call(httpMethod, url, params, data);
+			const result = HTTP.call(httpMethod, url, options);
 			if (result.statusCode === 200 && result.data && result.data.translation && result.data.translation.length > 0) {
 				return decodeURIComponent(result.data.translation);
 			}
